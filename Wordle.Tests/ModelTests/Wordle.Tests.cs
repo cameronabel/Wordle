583056e36190ba7f039cd0b173c49dc287584e9c
Cameron Abel <cameronabel@gmail.com>
2023-02-08 14:55:28 -0800

    boilerplate project setup

---
+++ Wordle.Tests/ModelTests/Wordle.Tests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Game.Models;
+
+namespace Game.Tests
+{
+  [TestClass]
+  public class ScoreTests
+  {
+
+  }
+}
---
+++ Wordle/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using Game.Models;
+
+namespace Game
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome, I am a Wordle clone!");
+    }
+  }
+}
